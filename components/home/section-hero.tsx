ff57f68f6ddd579fce6510fcdfc4bb0c6eda6de6
dcatcherex <dcatcherex@yahoo.com>
2024-03-20 16:11:20 +0700

    create toeicinfo components

--- components/home/section-hero.tsx
+++ components/home/section-hero.tsx
@@ -1,7 +1,28 @@
+import React, { useState } from 'react';
+
 import Image from "next/image";
 import { Button } from "../ui/button";
+import ToeicSchedule from "../toeicinfo/toeic-schedule";
+import ToeicReserve from "../toeicinfo/toeic-reserve";
+import ToeicReportto from "../toeicinfo/toeic-reportto";
+import ToeicProve from "../toeicinfo/toeic-prove";
+import ToeicExpense from "../toeicinfo/toeic-expense";
+import ToeicGetresult from "../toeicinfo/toeic-getresult";
+import ToeicPrepare from "../toeicinfo/toeic-prepare";
+import ToeicDirection from "../toeicinfo/toeic-direction";
 
 const supportIconSize = 40;
+
+const components = [
+  { component: <ToeicSchedule />, name: 'ToeicSchedule' },
+  { component: <ToeicReserve />, name: 'ToeicReserve' },
+  { component: <ToeicReportto />, name: 'ToeicReportto' },
+  { component: <ToeicProve />, name: 'ToeicProve' },
+  { component: <ToeicExpense />, name: 'ToeicExpense' },
+  { component: <ToeicGetresult />, name: 'ToeicGetresult' },
+  { component: <ToeicPrepare />, name: 'ToeicPrepare' },
+  { component: <ToeicDirection />, name: 'ToeicDirection' },
+];
 
 const SectionHero = () => {
   return (
@@ -111,49 +132,15 @@ const SectionHero = () => {
                 backgroundSize: 'cover',
                 // padding: '50px'
               }}>
-              <h3 className="mb-4">
-                ตารางสอบ Toeic
-              </h3>
-              <div className="h-[500px] overflow-y-auto">
-                <div className="mb-4">
-                  <div className="mb-2 font-semibold">ศูนย์สอบ: กรุงเทพฯ, เชียงใหม่</div>
-                  <p>
-                    TOEIC® Listening & Reading Test (Institutional Test)
-                  </p>
-                  <ul className="list-disc ml-5">
-                    <li>ประจำเดือนมีนาคม 2567: 1-30 มีนาคม 2567</li>
-                    <li>จันทร์-เสาร์</li>
-                    <li>รอบสอบ 2 รอบ คือ  รอบเช้า 9.00 น. และ รอบบ่าย 13.30 น.</li>
-                    <li>Check-in ได้ในเวลา 8.00-8.50 น. (รอบเช้า) และ 12.30-13.20 (รอบบ่าย)</li>
-                    <li>สามารถรับผลคะแนนในวันจันทร์-เสาร์ เวลา 14.30-16.30 น.</li>
-                  </ul>
-                </div>
-                <div className="mb-4">
-                  <div className="mb-2 font-semibold">ศูนย์สอบ: ชลบุรี</div>
-                  <p>
-                    TOEIC® Listening & Reading Test (Institutional Test)
-                  </p>
-                  <ul className="list-disc ml-5">
-                    <li>วันสอบ 23 มีนาคม 2567</li>
-                    <li>เปิดรับจองสอบระหว่างวันที่ 26 กุมภาพันธ์ 2567 – 21 มีนาคม 2567</li>
-                    <li>รอบสอบ 2 รอบ คือ  รอบเช้า 9.00 น. และ รอบบ่าย 14.00 น.</li>
-                    <li>Check-in ได้ในเวลา 8.00-8.45 น. (รอบเช้า) และ 13.00-13.45 (รอบบ่าย)</li>
-                  </ul>
-                </div>
-                <div className="mb-4">
-                  <div className="mb-2 font-semibold">ศูนย์สอบ: ระยอง</div>
-                  <p>
-                    TOEIC® Listening & Reading Test (Institutional Test)
-                  </p>
-                  <ul className="list-disc ml-5">
-                    <li>วันสอบ: รอประกาศจาก CPA</li>
-                    <li>เปิดรับจองสอบระหว่างวันที่: รอประกาศจาก CPA</li>
-                    <li>รอบสอบ: รอประกาศจาก CPA</li>
-                    <li>Check-in: รอประกาศจาก CPA</li>
-                  </ul>
-                  
-                </div>
-              </div>
+                <ToeicSchedule />
+                <ToeicReserve />
+                <ToeicReportto />
+                <ToeicProve />
+                <ToeicExpense />
+                <ToeicGetresult />
+                <ToeicPrepare />
+                <ToeicDirection />
+              
             </div>
 
           </div>
